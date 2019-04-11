29a2d9f8e4646921c5107631a325b2b3176a2011
Hunter Callaway <HunterCallaway@hotmail.com>
2019-04-10 21:49:26 -0600

    Completed the "FizzBuzz" exercise.

--- chapter-two.js
+++ chapter-two.js
@@ -20,3 +20,21 @@ Second Attempt
 
 for (let hashTag = "#"; hashTag.length <= 7; hashTag += "#")
 	console.log(hashTag);
+
+//FizzBuzz
+
+function fizzBuzz() {
+	for(let num = 1; num < 101; num++) {
+		if(num % 3 === 0 && num % 5 !== 0) {
+			console.log('Fizz');
+		} else if(num % 5 === 0 && num % 3 !== 0) {
+			console.log('Buzz');
+		} else if (num % 15 === 0) {
+			console.log('FizzBuzz');
+		} else {
+			console.log(num);
+		}
+	}
+}
+
+fizzBuzz();
