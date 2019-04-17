37e68c5f1afc658983f61c02643d2cf7d7569d60
Hunter Callaway <HunterCallaway@hotmail.com>
2019-04-16 21:26:44 -0600

    First attempt at the "Recursion" exercise.

--- chapter-three.js
+++ chapter-three.js
@@ -10,3 +10,17 @@ function minFinder (a, b) {
 
 minFinder(88, 33);
 minFinder(321, 123);
+
+//Recursion
+
+function isEven(num) {
+	if (num % 2 === 0) {
+		return true;
+	} else if (num % 2 === 1) {
+		return false;
+	} else {
+		return isEven(num -2);
+	}
+}
+
+isEven(44);
